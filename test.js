c9012a089ed59c26bb2cae2489d852f5aa03d52d
David Dikman <ddikman@gmail.com>
2025-02-20 11:14:22 +0100

    Fixed test

--- test.js
+++ test.js
@@ -7,10 +7,18 @@ describe('index.js', function() {
     const tester = new CliTest()
     const result = await tester.exec('node index.js ./example.json');
     const output = result.stdout;
-    expect(output).contains('anonymous: 1 total, 0 last month')
-    expect(output).contains('google.com: 2 total, 1 last month')
-    expect(output).contains('facebook.com: 1 total, 0 last month')
-    expect(output).contains('email: 1 total, 0 last month')
-    expect(output).contains('phone: 1 total, 0 last month')
+    const rows = output.split('\n')
+    try {
+      expect(rows).contains('anonymous: 1 total, 0 last month')
+      expect(rows).contains('google.com: 2 total, 0 last month')
+      expect(rows).contains('facebook.com: 1 total, 0 last month')
+      expect(rows).contains('email: 1 total, 0 last month')
+      expect(rows).contains('phone: 1 total, 0 last month')
+      expect(rows).contains('apple: 1 total, 0 last month')
+    } catch (err) {
+      console.log('Test failed. Full output was:')
+      console.log(output)
+      throw err
+    }
   })
 })
